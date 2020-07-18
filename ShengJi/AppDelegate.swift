07d962ca56b0f5dea1950321fa168e27fe186647
Ray Kim <rckim77@gmail.com>
2020-07-18 00:39:41 -0400

    restructures with podfile

---
+++ ShengJi/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  ShengJi
+//
+//  Created by Ray Kim on 7/17/20.
+//  Copyright © 2020 Ray Kim. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        window?.rootViewController = MenuViewController()
+        return true
+    }
+}
+
