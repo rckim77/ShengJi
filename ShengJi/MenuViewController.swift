41fb0d3d81ec9bdd6ea042d59be76da648174061
Ray Kim <rckim77@gmail.com>
2020-07-19 15:21:20 -0400

    adds basic join and create buttons

--- ShengJi/MenuViewController.swift
+++ ShengJi/MenuViewController.swift
@@ -7,12 +7,41 @@
 //
 
 import UIKit
+import SnapKit
 
 final class MenuViewController: UIViewController {
+    
+    private lazy var joinButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Join game", for: .normal)
+        button.titleLabel?.font = .preferredFont(forTextStyle: .title1)
+        button.setTitleColor(.darkGray, for: .normal)
+        return button
+    }()
+    
+    private lazy var createButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Create a game", for: .normal)
+        button.titleLabel?.font = .preferredFont(forTextStyle: .title1)
+        button.setTitleColor(.darkGray, for: .normal)
+        return button
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .green
+        view.backgroundColor = .white
+        view.addSubview(joinButton)
+        view.addSubview(createButton)
+        
+        joinButton.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.centerY.equalToSuperview().offset(-24)
+        }
+        
+        createButton.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(joinButton.snp.bottom).offset(8)
+        }
     }
 }
